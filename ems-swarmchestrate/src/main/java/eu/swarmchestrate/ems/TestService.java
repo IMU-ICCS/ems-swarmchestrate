897e516cfb5a2d7a3751165b900bc285246cbf4f
ipatini <ipatini@mail.ntua.gr>
2026-08-19 12:55:54 +0300

    Fixing runtime errors

--- ems-swarmchestrate/src/main/java/eu/swarmchestrate/ems/TestService.java
+++ ems-swarmchestrate/src/main/java/eu/swarmchestrate/ems/TestService.java
@@ -15,7 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Slf4j
-@Service
+//@Service
 public class TestService implements PostTranslationPlugin {
 	@Override
 	public void processTranslationResults(TranslationContext translationContext, TopicBeacon topicBeacon) {
